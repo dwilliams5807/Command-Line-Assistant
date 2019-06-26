0c2db129e246644995146ff88d6b6ee2cd778468
Daniel Williams <daniellanierwilliams@gmail.com>
2019-06-26 12:56:52 -0500

    added movie and concert search functions. Updated ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # liri-node-app
+Introduction
+
+LIRI is a Language Interpretation and Recognition Interface. LIRI is a command line node app that takes in commands and gives back data. Currently, it is able to search Spotify for songs, OMDB for movies & Bands in Town for concerts.
+
+Setup
+
+Clone the repository
+
+Run npm install, and the following packages should be installed:
+
+Node-Spotify-API
+
+Axios : This module will be used to get the IMDB and BandsInTown API data
+
+Moment
+
+DotEnv
+
+Create a .env file in the same directory as the rest of the files. In the .env file should be:
+
+'# Spotify API keys'
+
+'SPOTIFY_ID=your-spotify-ID-here'
+
+'SPOTIFY_SECRET=your-spotify-secret-here'
+
+
+
+TO USE:
+
+Search Song = node liri-app.js spotify-this SONGNAME
+
+Search Concert = node liri-app.js concert-this ARTIST
+
+Search Movie = node liri-app.js movie-this MOVIENAME
+
+Search Random Text File = node liri-app.js do-this
--- liri-app.js
+++ liri-app.js
@@ -26,6 +26,7 @@ var fs = require('fs'); //To read the random.txt file for the do-what-it-says fu
 var command = process.argv[2]; //For the switch statement
 var value = process.argv[3]; //To send the song/movie/concert to their respective functions
 
+function switchCase(command, value) {
 switch (command) {
     case "concert-this":
         concertThis(value);
@@ -38,12 +39,13 @@ switch (command) {
         break;
     case "do-this":
         doThis(value);
+        getRandom();
         break;
-};
-
+} };
+// Spotify Function
 function spotifySong(value) {
     if(!value){
-        value = "The Sign";
+        value = "Self Care";
     }
     spotify
     .search({ type: 'track', query: value })
@@ -58,3 +60,62 @@ function spotifySong(value) {
                     
             console.log(spotifyResults);
         }})}
+//   Bands Function 
+function concertThis(value) {
+    axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp")
+    .then(function(response) {    
+        for (var i = 0; i < response.data.length; i++) {
+
+            var datetime = response.data[i].datetime; //Saves datetime response into a variable
+            var dateArr = datetime.split('T'); //Attempting to split the date and time in the response
+
+            var concertResults = 
+                "--------------------------------------------------------------------" +
+                    "\nVenue Name: " + response.data[i].venue.name + 
+                    "\nVenue Location: " + response.data[i].venue.city +
+                    "\nDate of the Event: " + moment(dateArr[0], "MM-DD-YYYY"); //dateArr[0] should be the date separated from the time
+            console.log(concertResults);
+        }
+    })
+    .catch(function (error) {
+        console.log(error);
+    })};
+     // Movie Function
+    function movieThis(value) {
+        if(!value){
+            value = "Good Burger";
+        }
+        axios.get("https://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy")
+        .then(function(response) {
+                var movieResults = 
+                    "--------------------------------------------------------------------" +
+                        "\nMovie Title: " + response.data.Title + 
+                        "\nYear of Release: " + response.data.Year +
+                        "\nIMDB Rating: " + response.data.imdbRating +
+                        "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                        "\nCountry Produced: " + response.data.Country +
+                        "\nLanguage: " + response.data.Language +
+                        "\nPlot: " + response.data.Plot +
+                        "\nActors/Actresses: " + response.data.Actors;
+                console.log(movieResults);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+        
+    }
+    //text file function
+    function doThis(value) {
+    
+        fs.readFile("random.txt", "utf8", function(error, data) {
+            if (error) {
+                return console.log(error);
+            }
+            var dataArr = data.split(',');
+            spotifySong(dataArr[0], dataArr[1]);
+        })
+    }
+           
+
+
+            switchCase(command, value);
